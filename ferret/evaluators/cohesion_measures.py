Fixed parameters for compute segments calculation.

ferret/evaluators/cohesion_measures.py
@@ -123,7 +123,7 @@ class NSegments_CohesionEvaluation(BaseEvaluator):
                 human_n_segments = self._compute_n_segments(np.array(top_tokens_g))
 
                 top_tokens_r = get_discrete_explanation(score_explanation, v, only_pos, get_discrete_rationale_function)
-                rationale_n_segments = self._compute_n_segments(top_tokens_r)
+                rationale_n_segments = self._compute_n_segments(np.array(top_tokens_r))
 
                 n_segment_scores.append(self._compute_n_segments_score(human_n_segments, rationale_n_segments))
 